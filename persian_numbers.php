02a749336b7bac91d3e383f3913294ebb23c5996
Mohammad Kermani <m-kermani@users.noreply.github.com>
2016-03-14 23:34:46 +0330

    First push of Persian_number function

---
+++ persian_numbers.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Persian number
+ * This function will convert all English numbers to Persian (Arabic) numbers
+ * How to use: Persian_number(text to convert its number);
+ * Return: String
+ */
+function persian_number($text)
+{
+    $english = array('0','1','2','3','4','5','6','7','8','9');
+    $persian = array('۰','۱','۲','۳','۴','۵','۶','۷','۸','۹');
+
+    $text = str_replace($english, $persian, $text);
+    return $text;
+}
